319618f555c2545148d4134b5ad1bb0069f7a8ee
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2021-12-02 13:19:08 +0100

    C++ solves Day01 Part One

--- AoC++/aocDay01.h
+++ AoC++/aocDay01.h
@@ -10,7 +10,7 @@ int aocDay01()
 {
 	printString("aocDay01",2);
 
-	string filePath = filePathGen("01",2);
+	string filePath = filePathGen("01",0);
 
 	vector<string> vecStr;
 	vecStr = readFileToStringVector(filePath);
@@ -23,12 +23,29 @@ int aocDay01()
 	//Parameters
 	int ups = 0;
 	int downs = 0;
-	int notApp = 0;
+	int notApp = 0; //Short for Not Applicable
+
+	//The depth at each measue point
+	int depth = inputData[0];
 
 	//Checking the ups / downs in the data
-	for (int i = 0; i < inputData.size(); i++) {
-		printInt(inputData[i]);
+	for (int i = 1; i < inputData.size(); i++) {
+		if (depth < inputData[i]){
+			ups += 1;
+		}
+		else if (depth > inputData[i]) {
+			downs += 1;
+		}
+		else {
+			notApp += 0;
+		}
+
+		depth = inputData[i];
 	}
+
+	printInt(ups);
+	printInt(downs);
+	printInt(notApp);
 
 	return 0;
 }
