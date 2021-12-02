6cb9dde8cf415714d3a7fbd1e9772754e1e0b0fe
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2021-12-02 13:30:40 +0100

    C++ Solves Day 1

--- AoC++/aocDay01.h
+++ AoC++/aocDay01.h
@@ -25,22 +25,64 @@ int aocDay01()
 	int downs = 0;
 	int notApp = 0; //Short for Not Applicable
 
+	//inputData
+	vector<int> dataSet = inputData;
+
 	//The depth at each measue point
-	int depth = inputData[0];
+	int depth = dataSet[0];
 
 	//Checking the ups / downs in the data
-	for (int i = 1; i < inputData.size(); i++) {
-		if (depth < inputData[i]){
+	for (int i = 1; i < dataSet.size(); i++) {
+		if (depth < dataSet[i]){
 			ups += 1;
 		}
-		else if (depth > inputData[i]) {
+		else if (depth > dataSet[i]) {
 			downs += 1;
 		}
 		else {
 			notApp += 0;
 		}
 
-		depth = inputData[i];
+		depth = dataSet[i];
+	}
+
+	printInt(ups);
+	printInt(downs);
+	printInt(notApp);
+
+	printString("Part Two");
+
+	vector<int> processedData;
+
+	for (int i = 0; i < inputData.size() - 2; i++) {
+		int temp = inputData[i] + inputData[i + 1] + inputData[i + 2];
+		processedData.push_back(temp);
+	}
+
+	//Parameters
+	ups = 0;
+	downs = 0;
+	notApp = 0; //Short for Not Applicable
+
+	//inputData
+	dataSet = processedData;
+
+	//The depth at each measue point
+	depth = dataSet[0];
+
+	//Checking the ups / downs in the data
+	for (int i = 1; i < dataSet.size(); i++) {
+		if (depth < dataSet[i]) {
+			ups += 1;
+		}
+		else if (depth > dataSet[i]) {
+			downs += 1;
+		}
+		else {
+			notApp += 0;
+		}
+
+		depth = dataSet[i];
 	}
 
 	printInt(ups);
